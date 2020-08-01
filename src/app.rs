8ddbf86f4c7f215e138c3abb7ceced339b852ecc
Andrew Straw <strawman@astraw.com>
2020-08-01 10:47:29 +0200

    fix cargo clippy complaint

--- src/app.rs
+++ src/app.rs
@@ -358,14 +358,12 @@ impl App {
                         pix.color = rgb_u8;
                     }
 
-                    let new_data = web_sys::ImageData::new_with_u8_clamped_array_and_sh(
+                    web_sys::ImageData::new_with_u8_clamped_array_and_sh(
                         Clamped(data.as_mut_slice()),
                         w,
                         h,
                     )
-                    .unwrap();
-
-                    new_data
+                    .unwrap()
                 };
                 ctx2.put_image_data(&new_data, 0.0, 0.0).unwrap();
             }
