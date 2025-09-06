fcba513f564f7da859d89a675484eeaba7ccec41
Andrew Straw <strawman@astraw.com>
2025-09-05 21:56:47 -0400

    remove unused 'wee_alloc' features

--- hnb-app/src/lib.rs
+++ hnb-app/src/lib.rs
@@ -59,12 +59,6 @@ impl PositionInfo {
     }
 }
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 // This is the entry point for the web app
 #[wasm_bindgen]
 pub fn run_app() -> Result<(), JsValue> {
